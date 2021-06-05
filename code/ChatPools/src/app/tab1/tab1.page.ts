6403e62c5efe05a77b0ad72409a0dd3ab5e9262b
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2021-06-05 13:01:18 +0200

    Changed the look of some pages. Added user info.

--- code/ChatPools/src/app/tab1/tab1.page.ts
+++ code/ChatPools/src/app/tab1/tab1.page.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { User } from '../model/user';
+import { FauthService } from '../services/fauth.service';
+import { UserService } from '../services/user.service';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
+
 export class Tab1Page {
 
-  constructor() {}
+
+  //Variables
+  readMode: boolean = true;
+  currentUser: User;
+
+
+  constructor(
+    public userService: UserService,
+    private menu: MenuController,
+    private auth: FauthService,
+  ) {
+
+    //Get current user
+    this.getUser();
+  }
+
+
+  //Get Fire Store User
+  async getUser() {
+
+    //Get current FireAuth user
+    await this.auth.getCurrentUser().subscribe(
+
+      data => {
+
+        //Get the equivalent User on FireStore
+        this.userService.getUserByUid(data.uid).subscribe(
+
+          user => this.currentUser = user
+        )
+      }
+    );
+  }
 
 }
